41f11a694db2b540dccee8c2d84ffdbf07247572
Antti Hätinen <pharazon@phz.fi>
2021-11-30 07:39:34 +0200

    Added trailing slash

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,7 +5,7 @@ apt-get -y install software-properties-common
 add-apt-repository ppa:ondrej/nginx
 apt-get update
 apt-get -y install \
-    nginx-extras
+    nginx-extras \
     nano \
     less \
     git \
