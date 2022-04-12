8a5a08ee79c5fcd3690f493b25b32b5b4921ad50
Juhani Westman <juhani.westman@phz.fi>
2022-04-12 10:36:03 +0300

    Improvements to build.sh
    Fix argument parsing, proper word splitting and change from bash to sh.

--- docker/build.sh
+++ docker/build.sh
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+
 VERSION=$1
 USER=$2
 PASSWORD=$3
 
-if [ -z "$VERSION" ]; then
-  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password"
+if [ $# -ne 3 ]; then
+  echo 'Usage: ./docker/build.sh <version> <user> <password>, e.g. ./docker/build.sh latest user password'
   exit 1
 fi
 
-#login to docker hub
-docker login -u $USER -p $PASSWORD
+# login to docker hub
+docker login -u "$USER" -p "$PASSWORD"
 
 IMAGE=phzfi/ubuntu32-nginx:$VERSION
 LATEST=phzfi/ubuntu32-nginx:latest
 echo "Building $IMAGE"
 
-docker build --platform linux/686 -t $IMAGE -f Dockerfile . --no-cache
-docker tag $IMAGE $IMAGE
-docker tag $IMAGE $LATEST
-docker push $IMAGE
-docker push $LATEST
+docker build --platform linux/686 -t "$IMAGE" -f Dockerfile . --no-cache
+docker tag "$IMAGE" "$IMAGE"
+docker tag "$IMAGE" "$LATEST"
+docker push "$IMAGE"
+docker push "$LATEST"
 
