c71a562bd37f292bdadf13432aadaef09f638d98
Antti Hätinen <antti.hatinen@phz.fi>
2025-06-21 13:56:16 +0300

    Remove ubuntu and shell2junit since it's already installed

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
 apt-get update
-apt-get -y install software-properties-common
-add-apt-repository ppa:ondrej/nginx
-apt-get update
 apt-get -y install \
     nginx-extras \
     nano \
     less \
     git \
     wget \
-    curl \
-    bc # required by sh2ju
+    curl
 
-# Install sh2ju, a JUnit compliant test framework for Bash
-# See http://manolocarrasco.blogspot.fi/2010/02/hudson-publish-bach.html
-git -C /usr/src clone https://github.com/manolo/shell2junit.git
